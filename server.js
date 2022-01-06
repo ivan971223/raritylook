c0d0c1ef779760b2591177617b0d8d4116d0b0c3
Louis Chan <louis971223@gmail.com>
2022-01-06 19:09:23 +0800

    first commit hins

--- server.js
+++ server.js
@@ -184,7 +184,7 @@ if (process.env.NODE_ENV == "production"){
     app.use(express.static("nft_web/build"));
     const path = require("path")
     app.get("*", (req,res)=>{
-        res.sendFile(path.resolve(_dirname,'client','build','index.html'));
+        res.sendFile(path.resolve(_dirname,'nft_web','build','index.html'));
     })
 }
 //listener
