9c5e0bb88f10886c67d1e13c53c93e56689050b9
Louis Chan <louis971223@gmail.com>
2022-01-08 19:43:23 +0800

    35th change build

--- nft_web/src/axios.js
+++ nft_web/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://rarity-look.herokuapp.com'
+    baseURL: 'https://rarity-look.herokuapp.com'
 })
 
 export default instance
